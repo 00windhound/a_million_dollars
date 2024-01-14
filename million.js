b5de2783828f1c73463bd5aa1d765582bb2be66a
crystalyn <00crystalynthompson@gmail.com>
2024-01-13 20:22:38 -0700

    more stuff

--- million.js
+++ million.js
@@ -1,16 +1,12 @@
 let accountdisplay = document.getElementById('account')
 let account= 1000000
 accountdisplay.innerHTML=account
-const animaldisplay =document.getElementById('animals')
-const animals =[
-{name:'elephant', img:'pics/lilelephant.png'},
-{name:'giraffe', img:'pics/lilgiraffe.png'},
-{name:'flamingo', img:'pics/lilflamingo.png'},
-{name:'bunny', img:'pics/lilbunny.png'},
-{name:'lion', img:'pics/lillion.png'},
-{name:'snake', img:'pics/lilsnake.png'},
-]
 
-const lexus = document.getElementById('lexus')
-lexus.addEventListener('click', (account= account-50000))
+let shop = document.getElementById('shop');
 
+let generateshop =()=>{
+    return `
+    `
+};
+
+generateshop();
