777f280296ce2339b9ed8e0bd8d2443ae0a1d20d
crystalyn <00crystalynthompson@gmail.com>
2024-01-22 21:12:19 -0800

    stuff

--- cart.js
+++ cart.js
@@ -23,9 +23,23 @@ let generatecart =()=>{
         return (shoppingcart.innerHTML= basket.map((x)=> 
         {let {id,item}= x;
         let search = shopcarddata.find((y)=>y.id === id) || [];
-            return `<div class="shoppingcart">
-        <img src=${search.img} alt="" />
-        </div>`}).join(""));
+            return `
+        <div id=productid${search.id} class="cards";><div id="cardcontainer"><div id="cardmain">
+        <img width="220" alt="${search.alternate}" src=${search.img}>
+        <div class="details">
+            <h3>${search.name}</h3>
+            <p>${search.description}</p>
+            </div></div>
+            <div class="pq">
+                <h2>$ ${search.price}</h2>
+                <div class="pm">
+                 <button onclick="decrement(${search.id})" class="minus">-</button>
+                 <div id=${search.id} class="quantity" >${search.item === undefined ? 0: search.item}</div>
+                 <button onclick="increment(${search.id})" class="plus">+</button>
+                </div>
+            </div>
+        </div>
+    </div>`}).join(""));
     }
     else{
         shoppingcart.innerHTML = ``;
