d8c9c2e38eb9936131eae360e0352d660e399e87
crystalyn <00crystalynthompson@gmail.com>
2024-01-28 22:45:40 -0700

    i only deleted a ton of code over one error.. but! whats left is working.

--- cart.js
+++ cart.js
@@ -9,48 +9,23 @@ console.log(basket)
 
 let calculation = ()=>{
     let carticon = document.getElementsByClassName("totquantity");
-   // console.log( basket.map((x)=> x.item ).reduce((x,y)=> x + y, 0)); // did not want to work for me.
-
 carticon[0].innerHTML = (basket.map((x)=>x.item).reduce((x,y)=>x+y,0));
-console.log(basket.map((x)=>x.item).reduce((x,y)=>x+y,0));
-
-//need to fix this so it shows the number on the icon.
 };
+
 calculation();
 
 let generatecart =()=>{
     if(basket.length !== 0){
-       // console.log("data!!!")
-        return (shoppingcart.innerHTML= basket.map((x)=> 
-        {let {id,item}= x;
-        let search = shopcarddata.find((y)=>y.id === id) || []; //error with shopcarddata
+        console.log("data!!!")
+        return shoppingcart.innerHTML= basket.map((x)=>{
             return `
-            <div class="cartitem">
-                <img width="100px" src=${search.img} alt="${search.alternate}/>
-                <div class="details">
-                    <div class="titleprice">
-                        <h4 class="title"> <p>${search.name}</p> 
-                        <p class="itemp">$ ${search.price}</p>
-                        </h4>
-                       
-                    
-                        <div class="pm">
-                        <button onclick="decrement(${id})" class="minus">-</button>
-                        <div id=${id} class="quantity">${item}</div>
-                        <button onclick="increment(${id})" class="plus">+</button>
-                        </div>
-                     <button class="delete">delete</button>
-                     </div>
-                    <h3>$ ${item*search.item}</h3>
-                
-                </div>
-            </div>
-       `}).join(""));
+           <div>hellp</div>
+       `}).join("");
     }
     else{
         shoppingcart.innerHTML = ``;
         label.innerHTML= `<h2 id="cartisempty"> cart is empty </h2>
-                            <a href="shop.html" >
+                            <a href="animals.html" >
                             <button id="backtoshop">back to shop</button> </a>`
     }
 }
