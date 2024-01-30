241baad9427f4ccb19360653dea08c0b37a60133
crystalyn <00crystalynthompson@gmail.com>
2024-01-30 02:22:15 -0800

    about to test new card generation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let accountdisplay= document.getElementById("account");
 let account = JSON.parse(localStorage.getItem("monies"))
 let owns = JSON.parse(localStorage.getItem("owned"))
+let ownsarea= document.getElementById("owns");
 
 let setmonies =()=>{
     if (Object.is(account,null)){
@@ -14,5 +15,34 @@ let setmonies =()=>{
 }
 
 setmonies();
+
 console.log(account);
 accountdisplay.innerHTML=account;
+//i got the account working!!!
+
+let generateowned= () => {
+    if(owns.length !== 0){
+        return ownsarea.innerHTML= owns.map((x)=>{
+            let {id,item}=x;
+            let search=animalsdata.find((y)=>y.id === id) || [];
+            let {img,alternate,name}= search;
+            return`
+                <div class="ownedcards" id="${id}">
+                    <div class="ownedheader">
+                        <div class="ownedtitle"><b>${name}</b>
+                        </div>
+                        <div class="ownedamount">
+                            count 
+                            <div class="ownednumber"><b>${item}</b>
+                            </div> 
+                        </div>
+                    </div>
+                    <img width="300px" src="${img}" alt="${alternate}"></img>
+                </div>
+            `
+        }).join("")
+    }
+    else{
+        ownsarea.innerHTML=`<h1>you need to go shopping!</h1>`;
+    }
+}
